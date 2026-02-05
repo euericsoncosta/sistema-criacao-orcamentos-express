eeee76dda7ba167123e65d802812b48e3c7c4f9f
Ericson <costaericson@hotail.com>
2026-02-05 18:43:50 -0300

    Fix: usando env variables para o banco

--- src/config/database.cjs
+++ src/config/database.cjs
@@ -1,24 +1,3 @@
-// require("dotenv").config();
-
-// module.exports = {
-//   dialect: "mysql",
-//   host: process.env.DATABASE_HOST,
-//   port: process.env.DATABASE_PORT,
-//   username: process.env.DATABASE_USERNAME,
-//   password: process.env.DATABASE_PASSWORD,
-//   database: process.env.DATABASE,
-//   define: {
-//     timestamps: true,
-//     underscored: true,
-//     underscoredAll: true,
-//     createdAt: "created_at",
-//     updatedAt: "updated_at",
-//   },
-//   dialectOptions: {
-//     timezone: "+00:00", // Usando o formato UTC
-//   },
-//   timezone: "+00:00", // Usando o formato UTC
-// };
 require("dotenv").config();
 
 module.exports = {
@@ -33,12 +12,12 @@ module.exports = {
     underscored: true,
     underscoredAll: true,
     createdAt: "created_at",
-    updatedAt: "updated_at", // Note o 'A' maiúsculo em updatedAt para seguir o padrão
+    updatedAt: "updated_at",
   },
   dialectOptions: {
     ssl: {
       require: true,
-      rejectUnauthorized: false
+      rejectUnauthorized: false 
     },
     timezone: "+00:00",
   },
