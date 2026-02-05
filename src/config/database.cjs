f27629863798687e88d76149ea502c5c8821940f
Ericson <costaericson@hotail.com>
2026-02-05 18:31:03 -0300

    feat: estrutura inicial das telas de orçamentos (list, create, edit)

--- src/config/database.cjs
+++ src/config/database.cjs
@@ -1,3 +1,24 @@
+// require("dotenv").config();
+
+// module.exports = {
+//   dialect: "mysql",
+//   host: process.env.DATABASE_HOST,
+//   port: process.env.DATABASE_PORT,
+//   username: process.env.DATABASE_USERNAME,
+//   password: process.env.DATABASE_PASSWORD,
+//   database: process.env.DATABASE,
+//   define: {
+//     timestamps: true,
+//     underscored: true,
+//     underscoredAll: true,
+//     createdAt: "created_at",
+//     updatedAt: "updated_at",
+//   },
+//   dialectOptions: {
+//     timezone: "+00:00", // Usando o formato UTC
+//   },
+//   timezone: "+00:00", // Usando o formato UTC
+// };
 require("dotenv").config();
 
 module.exports = {
@@ -12,10 +33,14 @@ module.exports = {
     underscored: true,
     underscoredAll: true,
     createdAt: "created_at",
-    updatedAt: "updated_at",
+    updated_at: "updated_at",
   },
   dialectOptions: {
-    timezone: "+00:00", // Usando o formato UTC
+    ssl: {
+    require: true,
+    rejectUnauthorized: false // ESSENCIAL para a Vercel conectar ao Aiven
   },
-  timezone: "+00:00", // Usando o formato UTC
+    timezone: "+00:00",
+  },
+  timezone: "+00:00",
 };
